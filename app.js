71e5f0d79615abc881df23d122195b0ec72c609c
adhiariyadi <adhiariyadi40@gmail.com>
2020-06-16 12:43:47 +0700

    install cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 // import mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/staycation", {
@@ -37,6 +38,7 @@ app.use(
   })
 );
 app.use(flash());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
